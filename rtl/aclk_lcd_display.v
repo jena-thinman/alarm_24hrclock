8147183c788bb2ca218caf410a4857571085696f
jena-thinman <63758546+jena-thinman@users.noreply.github.com>
2020-11-18 18:34:04 -0800

    Update aclk_lcd_display.v

--- rtl/aclk_lcd_display.v
+++ rtl/aclk_lcd_display.v
@@ -1,31 +1,3 @@
-/********************************************************************************************
-
-Copyright 2018-2019 - Maven Silicon Softech Pvt Ltd. All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is considered a trade secret and is not to be divulged or used by parties who
-have not received written authorization from Maven Silicon Softech Pvt Ltd.
-
-Maven Silicon Softech Pvt Ltd
-Bangalore - 560076
-
-Webpage: www.maven-silicon.com
-
-Filename:	lcd_driver_4.v
-
-Description:	This module is the structural description of the
-                display unit which displays the 24hr format time
-		ms_hr ls_hr ms_min ls_min
-
-Date:		01/05/2018
-
-Author:		Maven Silicon
-
-Email:		online@maven-silicon.com
-
-Version:	1.0
-
-*********************************************************************************************/
 module aclk_lcd_display ( alarm_time_ms_hr,
                       alarm_time_ls_hr,
                       alarm_time_ms_min,
